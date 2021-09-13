093ef99acd4ec072dc5a803b912f733cb8053f18
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2021-09-13 15:17:49 -0400

    scavenger hunt

--- FSW-113-STUDENT/week5/assignment/jscript.js
+++ FSW-113-STUDENT/week5/assignment/jscript.js
@@ -1,14 +1,41 @@
 // Declare any necessary variables.
+let myStudentObj
 
 // Add am evemt listener that responds to the click of the "print" button by calling a function to instantiate
-//  a new student object, and another function to print the certificate.
+document.getElementById('print').addEventListener('click', function() {
+    //console.log('print button clicked!')
 
+    //  a new student object, and another function to print the certificate.
+    newStudent();
+    populateCertificate();
+
+    //console.log('Student Name: ', myStudentObj.studentName)
+    //console.log('Class Name: ', myStudentObj.className)
+    
+    
+})  
 // Add an event listener that responds to the click of the reset button by resetting all the values
 // both in the form and in the certificate.
+document.getElementById('reset').addEventListener('click', function(){
 
+})
 // Create a function that instantiates a new student object with the input from the HTML form.
+function newStudent() {
+    myStudentObj = new Student(
+        document.getElementById('studentName').value,
+        document.getElementById('className').value,
+        document.getElementById('studentScores').value,
+        document.getElementById('possibleScores').value
+    )
+}
 
 // Create a function that fills in the student's name, class name, and calculated grade on the certificate .
+function populateCertificate() {
+    document.getElementById('certStudentName').innerText = myStudentObj.studentName
+    document.getElementById('certClassName').innerText = myStudentObj.className
+    document.getElementById('certGrade').innerText = myStudentObj.letterGrade
+}
 
 // Create a function that converts the contents of a comma-separated text string to a numeric array.
+
 // You can use this function when instantiating the arrays in the student object.
--- FSW-113-STUDENT/week5/assignment/student.js
+++ FSW-113-STUDENT/week5/assignment/student.js
@@ -3,3 +3,51 @@
 // - a method that adds up all the student's scores
 // - a method that adds up all the possible scores
 // - a method that calculates the student's letter grade (divide the student's score by the possible scores and use the resulting decimal to determine grade)
+
+class Student {
+    constructor(args) {
+        this.studentName = arguments[0],
+        this.className = arguments[1], 
+        this.studentScores = arguments[2],
+        this.possibleScores = arguments[3]
+    }
+
+    letterGrade() {
+    // calculates the student's letter grade
+    let letterGrade = this.studentScore() / this.possibleScore()
+    switch(letterGrade) {
+        case (score < .6): 
+            grade = 'F'; break;
+        case (score < .7): 
+            grade = 'D'; break;
+        case (score < .8): 
+            grade = 'C'; break;
+        case (score < .9): 
+            grade = 'B'; break;
+        default: 
+            grade = 'A'; break;       
+    }
+}
+
+    studentScore() {
+        // adds up all student scores
+
+        let studentScore = this.studentScores.reduce((currentTotal, item) => {
+            return item + currentTotal
+        },
+        0
+    )
+        return studentScore
+}
+
+    possibleScore() {
+        // adds up all possible scores
+        let possibleScore = this.possibleScores.reduce((currentTotal, item) => {
+            return item + currentTotal
+        },
+        0
+        )
+        return possibleScore
+    }
+}
+
